56535e9b5c849ee26a9192610c4a957827c20a96
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-13 03:37:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def batch_badge_creator(name_list)
+  name_list.each do |name|
+    return "Hello, my name is #{name}"
+end
+
+def assign_rooms(name_list)
+  room = 1
+  while room < 8
+    "Hello, #{name_list[room-1]}! You'll be assigned to room #{room}!"
+  end
+end
